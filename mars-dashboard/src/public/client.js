f99eabab691eafc9c7148b618ce7029c53cdb4aa
Darya Iwako <darya.iwako@gmail.com>
2024-06-28 11:05:13 -0700

    Setting up API calls to NASA API

--- mars-dashboard/src/public/client.js
+++ mars-dashboard/src/public/client.js
@@ -24,18 +24,14 @@ const App = (state) => {
     return `
         <header></header>
         <main>
+            <h1>Mars Rovers Dashboard</h1>
             ${Greeting(store.user.name)}
             <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
+                <h3>Select rover to explore:</h3>
                 <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
+                    Rover information will go here.
                 </p>
+                <p> Photos will go here: </p>
                 ${ImageOfTheDay(apod)}
             </section>
         </main>
--- mars-dashboard/src/server/index.js
+++ mars-dashboard/src/server/index.js
@@ -12,14 +12,33 @@ app.use(bodyParser.json())
 
 app.use('/', express.static(path.join(__dirname, '../public')))
 
-// your API calls
-
-// example API call
-app.get('/apod', async (req, res) => {
+/* API calls to get information about a given Rover and photos on most recent date availabile */
+app.get('/roverinfo/:roverName', async (req, res) => {
     try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+        const roverInfoRaw = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${req.params.roverName}?api_key=${process.env.API_KEY}`)
             .then(res => res.json())
-        res.send({ image })
+        const roverInfo = {
+            "name": roverInfoRaw.photo_manifest.name,
+            "landing_date": roverInfoRaw.photo_manifest.landing_date,
+            "launch_date": roverInfoRaw.photo_manifest.launch_date,
+            "max_date": roverInfoRaw.photo_manifest.max_date,
+            "status": roverInfoRaw.photo_manifest.status
+        };
+
+        const roverPhotosRaw = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${req.params.roverName}/photos?earth_date=${roverInfo.max_date}&api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
+        console.log(roverPhotosRaw);
+        const roverPhotos = roverPhotosRaw.photos.map(photo => {
+            return {
+                "img_src": photo.img_src,
+                "earth_date": photo.earth_date,
+                "camera_name": photo.camera.full_name
+            }
+        });
+
+        roverInfo.photos = roverPhotos;
+
+        res.send(roverInfo);
     } catch (err) {
         console.log('error:', err);
     }
